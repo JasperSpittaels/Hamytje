dd40e973236e11dade235744c8a1803d0a428c08
JasperSpittaels <168077782+JasperSpittaels@users.noreply.github.com>
2026-05-17 14:17:08 +0200

    Update verstuur.php

--- verstuur.php
+++ verstuur.php
@@ -1,1 +1,11 @@
-
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $naam = $_POST['naam'];
+    $email = $_POST['email'];
+    $bericht = $_POST['bericht'];
+    
+    $naar = "info@uwwebsite.nl";
+    $onderwerp = "Nieuw bericht van " . $naam;
+    $headers = "From: " . $email;
+}
+?>
